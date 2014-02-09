e4937bef674ac0d421cab608e181ba7816607906
briches <briches@ualberta.ca>
2014-02-09 14:11:46 +0000

    Futureproofed this method a bit I think

--- python_RPI/serverPost.py
+++ python_RPI/serverPost.py
@@ -17,24 +17,34 @@ def postRequest(sn, count, delta):
 
 	return result
 
-# Because the camera does not seem to be working out, we generate random data
-# to use to test the windows/android/ios app
-dl = 100
-count = [[0 for i in xrange(4)] for i in xrange(dl)]
-delta = [[0 for i in xrange(4)] for i in xrange(dl)]
-for i in range(0,3):
-	for j in range(0,dl):
-		count[j][i] = (randint(1,10))
-		delta[j][i] = (uniform(0,1))
+useRandData = 1
 
+num_sn = int(sys.argv[1])
+currentID = 0;
+
+sn = dict([('nullf','nulld')])
+for i in range(0, num_sn):
+	currentID += 1
+	key = '0000000'+str(currentID)
+	key = str(key[8-len(key):])
+	sn[key] = {'count':0,'delta':0} 
+
+myKeys = sn.keys()
+
+
+		
 while 1:
-	for i in range(1,dl):
+	# Because the camera does not seem to be working out, we can generate random data
+	# to use to test the windows/android/ios app
+	if(useRandData==1):
+		for i in range(0, num_sn):
+			sn[myKeys[i]]['count'] = randint(1,10)
+			sn[myKeys[i]]['delta'] = uniform(0,1)	
+	currentID = 0
+	for i in range(0, num_sn):
+		currentID += 1
+		key = '0000000'+str(currentID)
+		key = str(key[8-len(key):])
+		print postRequest(key, sn[key]['count'], sn[key]['delta'])
 		time.sleep(3)
-		postRequest("00000002", str(count[i][1]), str(delta[i][1]))
-		time.sleep(3)
-		postRequest("00000001", str(count[i][0]), str(delta[i][0]))
-		time.sleep(3)
-		postRequest("00000003", str(count[i][2]), str(delta[i][2]))
-		time.sleep(3)
-		postRequest("00000004", str(count[i][3]), str(delta[i][3]))
 	
