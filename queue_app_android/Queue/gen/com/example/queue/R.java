30ee4796ae3e840748ffe5eb294e44c9e1b0962a
andyrewsu <and-y-rew@hotmail.com>
2014-02-08 19:48:25 -0700

    Separate Tab Fragments
    Amazing! The tabs aren't even the same anymore.

--- queue_app_android/Queue/gen/com/example/queue/R.java
+++ queue_app_android/Queue/gen/com/example/queue/R.java
@@ -24,14 +24,20 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_get=0x7f080003;
-        public static final int action_settings=0x7f080002;
+        public static final int action_get=0x7f080006;
+        public static final int action_settings=0x7f080005;
+        public static final int button1=0x7f080003;
         public static final int pager=0x7f080000;
+        public static final int ratingBar1=0x7f080004;
         public static final int section_label=0x7f080001;
+        public static final int seekBar1=0x7f080002;
     }
     public static final class layout {
         public static final int activity_home_screen=0x7f030000;
-        public static final int fragment_home_screen_dummy=0x7f030001;
+        public static final int fragment_coffee=0x7f030001;
+        public static final int fragment_food=0x7f030002;
+        public static final int fragment_grocery=0x7f030003;
+        public static final int fragment_home_screen_dummy=0x7f030004;
     }
     public static final class menu {
         public static final int home_screen=0x7f070000;
--- queue_app_android/Queue/src/com/example/queue/HomeSCreen.java
+++ queue_app_android/Queue/src/com/example/queue/HomeSCreen.java
@@ -4,6 +4,8 @@ import java.util.Locale;
 
 import android.app.ActionBar;
 import android.app.FragmentTransaction;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
@@ -44,6 +46,7 @@ public class HomeSCreen extends FragmentActivity implements ActionBar.TabListene
         // Set up the action bar.
         final ActionBar actionBar = getActionBar();
         actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#840000")));
 
         // Create the adapter that will return a fragment for each of the three
         // primary sections of the app.
@@ -113,11 +116,27 @@ public class HomeSCreen extends FragmentActivity implements ActionBar.TabListene
             // getItem is called to instantiate the fragment for the given page.
             // Return a DummySectionFragment (defined as a static inner class
             // below) with the page number as its lone argument.
-            Fragment fragment = new DummySectionFragment();
-            Bundle args = new Bundle();
-            args.putInt(DummySectionFragment.ARG_SECTION_NUMBER, position + 1);
-            fragment.setArguments(args);
-            return fragment;
+            
+        	Fragment fragment = null;
+        	Bundle args = new Bundle();
+        	switch (position) {
+        		case 0:
+	        		fragment = new coffeeSectionFragment();
+	        		args.putInt(coffeeSectionFragment.ARG_SECTION_NUMBER, position+3);
+	        		fragment.setArguments(args);
+	        		return fragment;
+        		case 1:
+            		fragment = new foodSectionFragment();
+            		args.putInt(foodSectionFragment.ARG_SECTION_NUMBER, position+2);
+            		fragment.setArguments(args);
+            		return fragment;
+        		case 2:
+            		fragment = new grocerySectionFragment();
+            		args.putInt(grocerySectionFragment.ARG_SECTION_NUMBER, position+1);
+            		fragment.setArguments(args);
+            		return fragment;
+        	}
+        	return(fragment);
         }
 
         @Override
@@ -145,20 +164,58 @@ public class HomeSCreen extends FragmentActivity implements ActionBar.TabListene
      * A dummy fragment representing a section of the app, but that simply
      * displays dummy text.
      */
-    public static class DummySectionFragment extends Fragment {
+    public static class coffeeSectionFragment extends Fragment {
         /**
          * The fragment argument representing the section number for this
          * fragment.
          */
         public static final String ARG_SECTION_NUMBER = "section_number";
 
-        public DummySectionFragment() {
+        public coffeeSectionFragment() {
         }
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_home_screen_dummy, container, false);
+            View rootView = inflater.inflate(R.layout.fragment_coffee, container, false);
+            TextView dummyTextView = (TextView) rootView.findViewById(R.id.section_label);
+            dummyTextView.setText(Integer.toString(getArguments().getInt(ARG_SECTION_NUMBER)));
+            return rootView;
+        }
+    }
+    public static class foodSectionFragment extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        public static final String ARG_SECTION_NUMBER = "section_number";
+
+        public foodSectionFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_food, container, false);
+            TextView dummyTextView = (TextView) rootView.findViewById(R.id.section_label);
+            dummyTextView.setText(Integer.toString(getArguments().getInt(ARG_SECTION_NUMBER)));
+            return rootView;
+        }
+    }
+    public static class grocerySectionFragment extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        public static final String ARG_SECTION_NUMBER = "section_number";
+
+        public grocerySectionFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_grocery, container, false);
             TextView dummyTextView = (TextView) rootView.findViewById(R.id.section_label);
             dummyTextView.setText(Integer.toString(getArguments().getInt(ARG_SECTION_NUMBER)));
             return rootView;
