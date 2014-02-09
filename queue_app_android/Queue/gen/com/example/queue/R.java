88fbb95bbf1372a80b72497d5836832d0f6dfe89
andyrewsu <and-y-rew@hotmail.com>
2014-02-08 20:52:28 -0700

    Search/Refresh Buttons
    They don't do anything.

--- queue_app_android/Queue/gen/com/example/queue/R.java
+++ queue_app_android/Queue/gen/com/example/queue/R.java
@@ -21,16 +21,20 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int ic_action_refresh=0x7f020000;
+        public static final int ic_action_search=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int action_get=0x7f080006;
+        public static final int action_get=0x7f080008;
+        public static final int action_refresh=0x7f080007;
+        public static final int action_search=0x7f080006;
         public static final int action_settings=0x7f080005;
         public static final int button1=0x7f080003;
+        public static final int listView1=0x7f080002;
         public static final int pager=0x7f080000;
         public static final int ratingBar1=0x7f080004;
         public static final int section_label=0x7f080001;
-        public static final int seekBar1=0x7f080002;
     }
     public static final class layout {
         public static final int activity_home_screen=0x7f030000;
@@ -44,6 +48,8 @@ public final class R {
     }
     public static final class string {
         public static final int action_get=0x7f050005;
+        public static final int action_refresh=0x7f050007;
+        public static final int action_search=0x7f050006;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int title_section1=0x7f050002;
