1a483f421d6a3fcea633a6bddfe779a7c6d3b20b
andyrewsu <and-y-rew@hotmail.com>
2014-02-08 19:02:32 -0700

    Queue Android App
    This is the current version of the app. Commits are hard.

--- queue_app_android/Queue/gen/com/example/queue/R.java
+++ queue_app_android/Queue/gen/com/example/queue/R.java
@@ -24,6 +24,7 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
+        public static final int action_get=0x7f080003;
         public static final int action_settings=0x7f080002;
         public static final int pager=0x7f080000;
         public static final int section_label=0x7f080001;
@@ -36,6 +37,7 @@ public final class R {
         public static final int home_screen=0x7f070000;
     }
     public static final class string {
+        public static final int action_get=0x7f050005;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int title_section1=0x7f050002;
