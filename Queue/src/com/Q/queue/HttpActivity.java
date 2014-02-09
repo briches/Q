684948be66ea37d96e6092a044a037c595978eca
IrisAmp <byue@ualberta.ca>
2014-02-09 12:38:33 -0700

    GET REKKD

--- Queue/src/com/Q/queue/HttpActivity.java
+++ Queue/src/com/Q/queue/HttpActivity.java
@@ -9,14 +9,20 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.annotation.SuppressLint;
 import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.util.Log;
@@ -30,20 +36,52 @@ public class HttpActivity extends Activity {
 	private static final String DEBUG_TAG = "HTTPGetActivity";
 	private TextView textView;
 	private ListView lView;
-
+	
+	String foo;
+	
     @SuppressLint("NewApi")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
     	super.onCreate(savedInstanceState);
-    	
-        ActionBar actionBar = getActionBar();
-        actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#840000")));
-    	
 		setContentView(R.layout.activity_http);
+		ActionBar actionBar = getActionBar();
+        actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#840000")));
+		clickHandler(textView);
+		JSONObject bar;
+		JSONArray jArray = null;
+		
+		//Intent intent = new Intent(android.content.Intent.ACTION_VIEW, 
+		//	    Uri.parse("http://maps.google.com/maps?saddr=53.545811,-113.498908&daddr=10200 102 Ave. NW Second Cup"));
+
+		Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("geo:<lat>,<long>?q=10200 102 Ave. NW Second Cup"));
+		startActivity(intent);
+		
+		startActivity(intent);
+    	/*
+		try {
+			bar = new JSONObject(foo);
+			jArray = bar.getJSONArray("devices");
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+
 		lView = (ListView) findViewById(R.id.listView1);
 		ArrayList<String> dunderlist = new ArrayList<String>();
-		dunderlist.add("YOU AND ME CAN RULE THIS CITY SPIDER MAN");
-		dunderlist.add("OR WE CAN JUST FIGHT TO THE DEATH");
+		
+        String[] dunderstrings = new String[jArray.length()];
+        for (int i = 0; i < jArray.length(); i++)
+        {
+        	try {
+        		JSONObject iHateThis = jArray.getJSONObject(i);
+        		//JSONObject iHateThisalso = iHateThis.getJSONObject("loc");
+        		//String iStillHateThis = iHateThisalso.getString("name");
+				//dunderstrings[i] = jArray.getJSONObject(i).getJSONObject("loc").getString("name");
+				//dunderstrings[i] = iStillHateThis;
+				//dunderlist.add(dunderstrings[i]);
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+        }
 		
 		ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
 				this,
@@ -52,9 +90,8 @@ public class HttpActivity extends Activity {
 		
 		lView.setAdapter(arrayAdapter);
 		
-		Log.d(DEBUG_TAG, "Hi there!");
+		//Log.d(DEBUG_TAG, "Hi there!"); */
 		
-		clickHandler(textView);
 	}
 
 	@Override
@@ -100,7 +137,7 @@ public class HttpActivity extends Activity {
         InputStream is = null;
         // Only display the first 500 characters of the retrieved
         // web page content.
-        int len = 500;
+        int len = 1024;
             
         try {
             URL url = new URL(myurl);
@@ -117,6 +154,7 @@ public class HttpActivity extends Activity {
 
             // Convert the InputStream into a string
             String contentAsString = readIt(is, len);
+            foo = contentAsString;
             Log.d(DEBUG_TAG, contentAsString);
             return contentAsString;
             
